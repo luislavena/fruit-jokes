4fd600f53c7a56e8d9a875703633a80c2d9e8bab
Luis Lavena <luislavena@gmail.com>
2025-01-04 17:24:21 +0100

    Bare minimum webapp to boot

--- src/main.cr
+++ src/main.cr
@@ -1,1 +1,20 @@
+# Festivus-inspired single-file micro JSON API
 
+require "json"
+require "toro"
+
+class WebApp < Toro::Router
+  def routes
+
+  end
+end
+
+begin
+  WebApp.run do |server|
+    puts "Listening on http://0.0.0.0:3000"
+    server.listen "::", 3000
+  end
+ensure
+end
+
+Fiber.yield
